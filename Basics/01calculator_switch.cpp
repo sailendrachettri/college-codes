52ce1a13ad2c625bf9bbcb77aedc5f0ca3f3a09e
sailendrachettri <sailendra9083@gmail.com>
2021-10-20 18:51:28 +0530

    claculator using switch statement

--- Basics/01calculator_switch.cpp
+++ Basics/01calculator_switch.cpp
@@ -1,9 +1,46 @@
-#include <iostream>
-using namespace std;
+#include <iostream.h>
+#include <conio.h>
 
-int main()
+void main()
 {
-    
+    int num1, num2;
+    char op;
 
-    return 0;
+    clrscr();
+
+    cout<< "Enter the value of num1: "<<endl;    
+    cin>>num1;
+
+    cout<< "Enter the value of num2: "<<endl;
+    cin>>num2;
+
+    cout<< "Enter the operator: "<<endl;
+    cin>>op;
+
+    switch(op){
+        case '+':
+            cout<< "The addition of "<<num1 <<" and "<<num2<<" is "<<num1+num2<<endl;
+            break;
+
+        case '-':
+            cout<< "The subtraction of "<<num1 <<" and "<<num2<<" is "<<num1-num2<<endl;
+            break;
+
+        case '*':
+            cout<< "The multiolication of "<<num1 <<" and "<<num2<<" is "<<num1*num2<<endl;
+            break;
+            
+        case '/':
+            cout<< "The division of "<<num1 <<" and "<<num2<<" is "<<num1/num2<<endl;
+            break;
+
+        case '%':
+            cout<< "The remainder of "<<num1 <<" and "<<num2<<" is "<<num1%num2<<endl;
+            break;
+        
+        default:
+            cout<< "Invalid Input. Please try once again :)"<<endl;
+    }
+
+    getch();
 }
