74d5d0df38f2fba567b17f9e3f7a6eb3269be4dc
sailendrachettri <sailendra9083@gmail.com>
2021-10-25 21:54:17 +0530

    prime number or not

---
+++ Basics/prime_number.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    bool flag = true;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num <= 1)
+    {
+        cout << num << " is not prime" << endl;
+    }
+
+    else
+    {
+        for (int i = 2; i <= num / 2; i++)
+        {
+            if (num % i == 0)
+            {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag == true)
+            cout << num << " is prime number." << endl;
+        else
+            cout << num << " is not a prime number." << endl;
+    }
+
+    return 0;
+}
