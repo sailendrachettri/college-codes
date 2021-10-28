cd8fa2f946c2965b299030d1698b481e01db7b21
sailendrachettri <sailendra9083@gmail.com>
2021-10-28 08:05:58 +0530

    calc using switch

--- Basics1/calc_switch.cpp
+++ Basics1/calc_switch.cpp
@@ -3,7 +3,44 @@ using namespace std;
 
 int main()
 {
-    
+    int num1, num2;
+    char op;
+
+    cout<< "Enter the value of num1: "<<endl;     
+    cin>>num1;
+
+    cout<< "Enter the value of num2: "<<endl;     
+    cin>>num2;
+
+    cout<< "Enter the operator (eg: + - / * %): "<<endl;     
+    cin>>op;
+
+    switch(op){
+        case '+':
+            cout<< "The addition of "<<num1<<" and "<<num2<<" is "<<num1 + num2<<"."<<endl;
+            break;
+
+        case '-':
+            cout<< "The subtraction of "<<num1<<" and "<<num2<<" is "<<num1 - num2<<"."<<endl;
+            break;
+
+        case '/':
+            cout<< "The division of "<<num1<<" and "<<num2<<" is "<<num1  /num2<<"."<<endl;
+            break;
+
+        case '*':
+            cout<< "The multiplication of "<<num1<<" and "<<num2<<" is "<<num1 * num2<<"."<<endl;
+            break;
+
+        case '%':
+            cout<< "The remainder of "<<num1<<" and "<<num2<<" is "<<num1  % num2<<"."<<endl;
+            break;
+        
+        default:
+            cout<< "Something went wrong. Try again please :("<<endl;
+    }
+
+
 
     return 0;
 }
