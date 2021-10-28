ee352c11ff19eb24c658b838960aa261b4f2640a
sailendrachettri <sailendra9083@gmail.com>
2021-10-28 08:31:45 +0530

    clear some bugs

--- Basics1/prime_n1_n2.cpp
+++ Basics1/prime_n1_n2.cpp
@@ -17,7 +17,7 @@ int main()
         {
             int flag = true;
 
-            for (int i = 2; i < n / 2; i++)
+            for (int i = 2; i <= n / 2; i++)
             {
                 if (n % i == 0)
                 {
