ccce18965c53bcc7c3fbe60c775cce5421e5bc54
sailendrachettri <sailendra9083@gmail.com>
2021-10-28 08:25:48 +0530

    prime number from n1 to n2

---
+++ Basics1/prime_n1_n2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+
+    cout << "Enter the first number: " << endl;
+    cin >> num1;
+
+    cout << "Enter the second number: " << endl;
+    cin >> num2;
+
+    if (num1 > 1 and num2 > 1)
+    {
+        for (int n = num1; n < num2; n++)
+        {
+            int flag = true;
+
+            for (int i = 2; i < n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag)
+                cout << n << " ";
+        }
+    } else{
+        cout<< "No prime number left behind."<<endl;
+    }
+
+    return 0;
+}
