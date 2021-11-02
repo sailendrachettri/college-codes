b1193c136778a799b65fc15a80400e8dd7d87876
sailendrachettri <sailendra9083@gmail.com>
2021-11-02 08:00:00 +0530

    palindrome number - but this is not right approach

---
+++ Practice/palindrome_number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string num = "313";
+    string temp = num;
+    reverse(temp.begin(), temp.end());
+
+    if(num == temp) cout<< num<<" is palidrome number."<<endl;
+    else cout<< num<<" is not a palidrome number."<<endl;
+
+
+
+    return 0;
+}
