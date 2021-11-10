e04e95b825b05f856019ca86d1f0df68d9f09d3f
sailendrachettri <sailendra9083@gmail.com>
2021-11-10 05:41:45 +0530

    array 1d

---
+++ Basics2/array_.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[4] = {10, 23, 43, 56};
+
+    for (int i = 0; i < 4; i++)
+    {
+        cout<< arr[i]<<" ";
+    }
+    
+
+    return 0;
+}
