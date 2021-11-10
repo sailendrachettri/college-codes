580e0e621eae9ec174fd4d9665f5e7ce594a97a0
sailendrachettri <sailendra9083@gmail.com>
2021-11-10 05:52:17 +0530

    multidimensional array

--- Basics2/array_.cpp
+++ Basics2/array_.cpp
@@ -12,20 +12,36 @@ int main()
     // }
 
     // 2 Dimensional array
-    int arr[3][3] = { {10, 20, 30},
-                      {40, 50, 60},
-                      {70, 80, 90}
-    };
+    // int arr[3][3] = { {10, 20, 30},
+    //                   {40, 50, 60},
+    //                   {70, 80, 90}
+    // };
+
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     for (int j = 0; j < 3; j++)
+    //     {
+    //         cout<< arr[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // multi dimensional array
+    int arr[3][3][3] = {{{10, 20, 30}, {40, 50, 60}, {70, 80, 90}},
+                        {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
+                        {{90, 80, 70}, {60, 50, 40}, {30, 20, 10}}};
 
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
-            cout<< arr[i][j]<<" ";
+            for (int k = 0; k < 3; k++)
+            {
+                cout << arr[i][j][k] << " ";
+            }
+            cout << endl;
         }
-        cout<<endl;
     }
-    
 
     return 0;
 }
