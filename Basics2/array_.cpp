1f2b78cc4fc3942b7073121bb819e1e95b95bbe6
sailendrachettri <sailendra9083@gmail.com>
2021-11-10 05:46:47 +0530

    2d array

--- Basics2/array_.cpp
+++ Basics2/array_.cpp
@@ -3,11 +3,27 @@ using namespace std;
 
 int main()
 {
-    int arr[4] = {10, 23, 43, 56};
+    // 1 Dimensional array
+    // int arr[4] = {10, 23, 43, 56};
 
-    for (int i = 0; i < 4; i++)
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     cout<< arr[i]<<" ";
+    // }
+
+    // 2 Dimensional array
+    int arr[3][3] = { {10, 20, 30},
+                      {40, 50, 60},
+                      {70, 80, 90}
+    };
+
+    for (int i = 0; i < 3; i++)
     {
-        cout<< arr[i]<<" ";
+        for (int j = 0; j < 3; j++)
+        {
+            cout<< arr[i][j]<<" ";
+        }
+        cout<<endl;
     }
     
 
