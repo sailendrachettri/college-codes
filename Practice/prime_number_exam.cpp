821a99f50297b8b8ef64f83f0c74970aa4d7ba71
sailendrachettri <sailendra9083@gmail.com>
2021-11-18 06:31:55 +0530

    prime number exam

---
+++ Practice/prime_number_exam.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 12; i <= 50; i++)
+    {
+        int c = 1;
+
+        for (int j = 2; j < i; j++)
+        {
+            if(i % j == 0){
+                c++;
+                break;
+            }
+        }
+
+        if(c == 1)
+            cout<< i<<" ";
+        
+    }
+        
+
+    return 0;
+}
